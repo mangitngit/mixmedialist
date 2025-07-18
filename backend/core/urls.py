f5a9c5d4256aba11b2b364d32a20a58b6937490e
Maciej Szpiech <maciej.lukasz.szpiech@gmail.com>
2025-07-18 21:11:41 +0200

    Project PURGE

--- backend/core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from api.views import hello
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/hello/', hello),
-]
